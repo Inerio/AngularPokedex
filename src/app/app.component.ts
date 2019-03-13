35d837e154ca13d5a37cdffabec98eba7deaf805
root <julien.dearaujo@epsi.fr>
2019-03-13 16:50:59 +0100

    Ajout du lien à la BDD firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'AngularPokedex';
+  constructor() {
+    var config = {
+      apiKey: "AIzaSyARChPTXCzOFjRpOFvYHRn5L0BLoeY2h1o",
+      authDomain: "angularpokedex-927e4.firebaseapp.com",
+      databaseURL: "https://angularpokedex-927e4.firebaseio.com",
+      projectId: "angularpokedex-927e4",
+      storageBucket: "angularpokedex-927e4.appspot.com",
+      messagingSenderId: "876537076915"
+    };
+    firebase.initializeApp(config);
+  }
 }
