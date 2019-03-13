6119fc7746e4c2584813e82cf417f742cf66a3f1
root <julien.dearaujo@epsi.fr>
2019-03-13 15:39:05 +0100

    Création des composents et ajout de bootstrap font-awesome

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { SigninComponent } from './auth/signin/signin.component';
+import { PokemonListComponent } from './pokemon-list/pokemon-list.component';
+import { SinglePokemonComponent } from './pokemon-list/single-pokemon/single-pokemon.component';
+import { PokemonFormComponent } from './pokemon-list/pokemon-form/pokemon-form.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    SigninComponent,
+    PokemonListComponent,
+    SinglePokemonComponent,
+    PokemonFormComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
