2b1b6425fe113be2eecb5a841c52ee6fa44a7267
root <julien.dearaujo@epsi.fr>
2019-03-14 12:21:59 +0100

    Ajout du Auth-guard et Opti du path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,16 @@ import { SigninComponent } from './auth/signin/signin.component';
 import { PokemonListComponent } from './pokemon-list/pokemon-list.component';
 import { PokemonFormComponent } from './pokemon-list/pokemon-form/pokemon-form.component';
 import { SinglePokemonComponent } from './pokemon-list/single-pokemon/single-pokemon.component';
+import { AuthGuardService } from './services/auth-guard.service';
 
 const routes: Routes = [
   { path: 'auth/signup', component: SignupComponent },
   { path: 'auth/signin', component: SigninComponent },
-  { path: 'pokemon', component: PokemonListComponent },
-  { path: 'pokemon/new', component: PokemonFormComponent },
-  { path: 'pokemon/view/:id', component: SinglePokemonComponent }
+  { path: 'pokemon', canActivate: [AuthGuardService], component: PokemonListComponent },
+  { path: 'pokemon/new', canActivate: [AuthGuardService], component: PokemonFormComponent },
+  { path: 'pokemon/view/:id', canActivate: [AuthGuardService], component: SinglePokemonComponent },
+  { path: '', redirectTo: 'pokemon', pathMatch: 'full' },
+  { path: '**', redirectTo: 'pokemon' }
 ];
 
 @NgModule({
